e72827d9262d641b66af948e8748d60adac97182
Arlandia Cardoso <lanny.cardoso@hotmail.com>
2018-05-23 20:08:05 -0300

    Adição da pasta primeiro_projeto

---
+++ PrimeiroProjeto/src/classes/PrimeiraClasse.java
@@ -0,0 +1,50 @@
+package classes;
+
+public class PrimeiraClasse {
+     // Comentario de linha
+	
+	/*
+	 * Comentario de bloco
+	 */ 
+	
+	//int a; //atributo 
+	
+	//int c;
+	
+	public static void main(String[] args) {
+		/*
+		int b; 
+		
+		int a;
+		a = 10;
+		
+		
+		c = 75; 
+		
+		*/
+		
+		int a = 11;
+		double  b = 5.5;
+		
+		//Por padrão a linguagem não aceita essa atribuição
+		//int j = b;
+		
+		char c= 'p';
+		char outrochar = '2'; 
+		
+		String str = "abacaxi";
+		
+		System.out.println("Quero imprimir isso!");
+		System.out.println(78 + 22);
+		System.out.println('c');
+		System.out.println(str);
+		
+		
+	
+		
+		
+		
+		
+	}
+
+}
